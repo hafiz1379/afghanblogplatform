f50def645a217246aff26fcadb2851d4ce239d85
hafiz1379 <hafizrasa1379@gmail.com>
2025-10-29 14:12:32 +0430

    integrate admin and protected routes into main App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,113 @@
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { AuthProvider } from "./context/AuthContext";
+import { PostProvider } from "./context/PostContext";
+import ProtectedRoute from "./components/ProtectedRoute";
+import AdminRoute from "./components/AdminRoute";
+import { UserProvider } from "./context/UserContext";
+import AdminUsers from "./pages/AdminUsers";
+import AdminPosts from "./pages/AdminPosts";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Posts from "./pages/Posts";
+import PostDetail from "./pages/PostDetail";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import CreatePost from "./pages/CreatePost";
+import MyPosts from "./pages/MyPosts";
+import EditPost from "./pages/EditPost";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold text-center">Welcome to the App</h1>
-    </div>
+    <AuthProvider>
+      <UserProvider>
+        {" "}
+        {/* اپلیکیشن را با UserProvider بپوشانید */}
+        <PostProvider>
+          <Router>
+            <div className="min-h-screen flex flex-col">
+              <Header />
+              <main className="flex-grow pt-16">
+                <Routes>
+                  {/* ... مسیرهای قبلی */}
+                  <Route path="/" element={<Home />} />
+                  <Route path="/posts" element={<Posts />} />
+                  <Route path="/posts/:id" element={<PostDetail />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/register" element={<Register />} />
+
+                  {/* مسیرهای محافظت شده */}
+                  <Route
+                    path="/dashboard"
+                    element={
+                      <ProtectedRoute>
+                        <Dashboard />
+                      </ProtectedRoute>
+                    }
+                  />
+                  <Route
+                    path="/profile"
+                    element={
+                      <ProtectedRoute>
+                        <Profile />
+                      </ProtectedRoute>
+                    }
+                  />
+                  <Route
+                    path="/create-post"
+                    element={
+                      <ProtectedRoute>
+                        <CreatePost />
+                      </ProtectedRoute>
+                    }
+                  />
+                  <Route
+                    path="/my-posts"
+                    element={
+                      <ProtectedRoute>
+                        <MyPosts />
+                      </ProtectedRoute>
+                    }
+                  />
+                  <Route
+                    path="/edit-post/:id"
+                    element={
+                      <ProtectedRoute>
+                        <EditPost />
+                      </ProtectedRoute>
+                    }
+                  />
+
+                  {/* مسیرهای ادمین */}
+                  <Route
+                    path="/admin/users"
+                    element={
+                      <AdminRoute>
+                        <AdminUsers />
+                      </AdminRoute>
+                    }
+                  />
+                  <Route
+                    path="/admin/posts"
+                    element={
+                      <AdminRoute>
+                        <AdminPosts />
+                      </AdminRoute>
+                    }
+                  />
+                </Routes>
+              </main>
+              <Footer />
+            </div>
+            <Toaster position="top-right" />
+          </Router>
+        </PostProvider>
+      </UserProvider>
+    </AuthProvider>
   );
 }
 
