cbc05b476c718623030c2e29f3ee7dee142e8c37
hafiz1379 <hafizrasa1379@gmail.com>
2025-11-10 10:39:24 +0430

    add About page and conditional Footer rendering

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { AuthProvider } from "./context/AuthContext";
 import { PostProvider } from "./context/PostContext";
@@ -11,6 +11,7 @@ import AdminPosts from "./pages/AdminPosts";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import About from "./pages/About";
 import Posts from "./pages/Posts";
 import PostDetail from "./pages/PostDetail";
 import Login from "./pages/Login";
@@ -21,88 +22,98 @@ import MyPosts from "./pages/MyPosts";
 import EditPost from "./pages/EditPost";
 import Profile from "./pages/Profile";
 
+// Wrapper component to conditionally show Footer
+const Layout = ({ children }) => {
+  const location = useLocation();
+  const noFooterPaths = ["/login", "/register"];
+  const showFooter = !noFooterPaths.includes(location.pathname);
+
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-grow pt-16">{children}</main>
+      {showFooter && <Footer />}
+    </div>
+  );
+};
+
 function App() {
   return (
     <AuthProvider>
       <UserProvider>
-        {" "}
-        {/* Wrap the application with UserProvider */}
         <PostProvider>
           <Router>
-            <div className="min-h-screen flex flex-col">
-              <Header />
-              <main className="flex-grow pt-16">
-                <Routes>
-                  {/* Public routes */}
-                  <Route path="/" element={<Home />} />
-                  <Route path="/posts" element={<Posts />} />
-                  <Route path="/posts/:id" element={<PostDetail />} />
-                  <Route path="/login" element={<Login />} />
-                  <Route path="/register" element={<Register />} />
+            <Layout>
+              <Routes>
+                {/* Public routes */}
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/posts" element={<Posts />} />
+                <Route path="/posts/:id" element={<PostDetail />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
 
-                  {/* Protected routes */}
-                  <Route
-                    path="/dashboard"
-                    element={
-                      <ProtectedRoute>
-                        <Dashboard />
-                      </ProtectedRoute>
-                    }
-                  />
-                  <Route
-                    path="/profile"
-                    element={
-                      <ProtectedRoute>
-                        <Profile />
-                      </ProtectedRoute>
-                    }
-                  />
-                  <Route
-                    path="/create-post"
-                    element={
-                      <ProtectedRoute>
-                        <CreatePost />
-                      </ProtectedRoute>
-                    }
-                  />
-                  <Route
-                    path="/my-posts"
-                    element={
-                      <ProtectedRoute>
-                        <MyPosts />
-                      </ProtectedRoute>
-                    }
-                  />
-                  <Route
-                    path="/edit-post/:id"
-                    element={
-                      <ProtectedRoute>
-                        <EditPost />
-                      </ProtectedRoute>
-                    }
-                  />
+                {/* Protected routes */}
+                <Route
+                  path="/dashboard"
+                  element={
+                    <ProtectedRoute>
+                      <Dashboard />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/profile"
+                  element={
+                    <ProtectedRoute>
+                      <Profile />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/create-post"
+                  element={
+                    <ProtectedRoute>
+                      <CreatePost />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/my-posts"
+                  element={
+                    <ProtectedRoute>
+                      <MyPosts />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/edit-post/:id"
+                  element={
+                    <ProtectedRoute>
+                      <EditPost />
+                    </ProtectedRoute>
+                  }
+                />
 
-                  {/* Admin routes */}
-                  <Route
-                    path="/admin/users"
-                    element={
-                      <AdminRoute>
-                        <AdminUsers />
-                      </AdminRoute>
-                    }
-                  />
-                  <Route
-                    path="/admin/posts"
-                    element={
-                      <AdminRoute>
-                        <AdminPosts />
-                      </AdminRoute>
-                    }
-                  />
-                </Routes>
-              </main>
-              <Footer />
-            </div>
+                {/* Admin routes */}
+                <Route
+                  path="/admin/users"
+                  element={
+                    <AdminRoute>
+                      <AdminUsers />
+                    </AdminRoute>
+                  }
+                />
+                <Route
+                  path="/admin/posts"
+                  element={
+                    <AdminRoute>
+                      <AdminPosts />
+                    </AdminRoute>
+                  }
+                />
+              </Routes>
+            </Layout>
             <Toaster position="top-right" />
           </Router>
         </PostProvider>
